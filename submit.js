80063f1a1c59bb5d8f14c9cb7f59c69e85279ddd
Tammy Lin <tammy@Tammys-MacBook-Air.local>
2022-04-17 11:29:28 -0400

    testing insert donor

--- submit.js
+++ submit.js
@@ -5,11 +5,34 @@ function handleSubmit(event) {
     const data = new FormData(event.target);
 
     const value = Object.fromEntries(data.entries());
+    const jsonOb = {
+        type: "INSERT",
+        data: { 
+            table: "DONOR",
+            firstname: data.entries().firstname,
+            lastname: data.entries().lastname,
+            phone: data.entries().phone,
+            iron_count: data.entries().iron_count,
+            age: data.entries().age
+        },
+        conditions: ""
+       
+}
+
 
     console.log({
-        value
+        jsonOb
     });
+
+     insertDonor(jsonOb);
 }
+
+
+                                
+
+
+
+
 
 const form = document.querySelector('form');
 form.addEventListener('submit', handleSubmit);
